88673342a40c8e1b19569e117b42be801961037d
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-04-20 17:47:36 +0600

    dragon news basic home page using bootstrap

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAzevtu2FY-xatQi34Nk71jZbju4a4IKmM",
+  authDomain: "the-news-dragon-fddeb.firebaseapp.com",
+  projectId: "the-news-dragon-fddeb",
+  storageBucket: "the-news-dragon-fddeb.appspot.com",
+  messagingSenderId: "571109404812",
+  appId: "1:571109404812:web:4dae6188b9aefad13f0ba9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
