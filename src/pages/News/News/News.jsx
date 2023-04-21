d0b40848688648af1c42a5d9d166132bc6bf0e23
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-04-21 21:53:55 +0600

    display news details video-5

--- src/pages/News/News/News.jsx
+++ src/pages/News/News/News.jsx
@@ -1,9 +1,28 @@
 import React from "react";
+import { Button, Card } from "react-bootstrap";
+import { FaArrowLeft } from "react-icons/fa";
+import { Link, useLoaderData } from "react-router-dom";
+import EditorsInsides from "../EditorsInsides/EditorsInsides";
 
 const News = () => {
+  const news = useLoaderData();
+  const { title, details, category_id, image_url } = news;
   return (
     <div>
-      <h2>News details coming soon</h2>
+      <Card>
+        <Card.Img variant="top" src={image_url} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>{details}</Card.Text>
+          <Link to={`/category/${category_id}`}>
+            <Button variant="danger">
+              {" "}
+              <FaArrowLeft></FaArrowLeft> All News In This Category
+            </Button>
+          </Link>
+        </Card.Body>
+      </Card>
+      <EditorsInsides></EditorsInsides>
     </div>
   );
 };
