6bca145d417795c84ba5ee95a3e140ec894a584b
Rakibulislammridha <dr.rakibulislammridha@gmail.com>
2023-04-29 18:21:41 +0600

    private routing is done properly video-4

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,12 +1,18 @@
 import React, { useContext } from "react";
 import { Button, Container, Form } from "react-bootstrap";
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../../providers/AuthProvider";
 
 const Login = () => {
   const { signIn } = useContext(AuthContext);
 
   const navigate = useNavigate();
+
+  const location = useLocation();
+
+  const from = location.state?.from?.pathname || "/category/0";
+
+  console.log("login page location", location);
 
   const handleLogin = (event) => {
     event.preventDefault();
@@ -19,7 +25,7 @@ const Login = () => {
       .then((result) => {
         const loggedUser = result.user;
         console.log(loggedUser);
-        navigate("/category/0");
+        navigate(from, { replace: true });
       })
       .catch((error) => {
         console.log(error);
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,21 +1,28 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router-dom";
 
-const PrivateRoute = () => {
+const PrivateRoute = ({ children }) => {
   const { user } = useContext(AuthContext);
+  const location = useLocation();
+  //   console.log(location);
 
-  return;
-  <></>;
+  if (user) {
+    return children;
+  }
+  return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
 
 export default PrivateRoute;
 
 /**
- *
+ * --------------
  * Steps
+ * --------------
  *
  * 1. Check user is logged in or not
  * 2. If user is logged in then allow them to visit the route
  * 3. Else redirect the user to the login page
+ * 4. Setup the private router
  *
  */
